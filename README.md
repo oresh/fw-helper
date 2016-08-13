d3491162e83f84a0eeca9f2a90739b31de2ba142
Kirill Borzov <oresh.work@gmail.com>
2016-08-13 13:54:56 +0300

    issue: major changes.

---
+++ README.md
@@ -0,0 +1,2 @@
+TODO: Understand how to proparly add fast jquery functions and snippets. Perhaps create 2 separate modules for that.
+
---
+++ index.js
@@ -0,0 +1,7 @@
+var fastjQ = require('lib/fastjq');
+var snippets = require('lib/snippets');
+
+module.export.Helper = {
+	jQ : fastjQ,
+	behavior: snippets
+};
